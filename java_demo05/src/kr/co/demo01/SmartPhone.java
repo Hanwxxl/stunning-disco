88895e76b8befa3a7b09035184f69f1a3580ea7d
user1 <user1@host.docker.internal>
2022-11-16 09:36:35 +0900

    객체, 인스턴스화, 생성자, 접근제한자, getter/setter메서드

---
+++ java_demo05/src/kr/co/demo01/SmartPhone.java
@@ -0,0 +1,32 @@
+package kr.co.demo01;
+
+public class SmartPhone {
+	private String model;
+	private String brand;
+	private String number;
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public String getBrand() {
+		return this.brand;
+	}
+	
+	public String getNumber() {
+		String num = this.number.substring(0, 9);
+		return num + "xxxx";
+	}
+}

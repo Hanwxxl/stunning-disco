df1bcca474ef09d0c28cdb2fa73f2a25c2d7312c
user1 <user1@host.docker.internal>
2022-11-16 09:56:25 +0900

    상속, 메서드 오버라이딩, 다형성 연습문제

---
+++ java_demo06/src/kr/co/exam03/Senior.java
@@ -0,0 +1,29 @@
+package kr.co.exam03;
+
+public class Senior extends Staff {
+
+	public Senior() {
+		role = "대리";
+	}
+	
+	public Senior(String name, int age) {
+		this();
+		setName(name);
+		setAge(age);
+	}
+	
+	public void call() {
+		String message = String.format("%s이(가) 업체와 통화를 합니다.", role);
+		System.out.println(message);
+	}
+	
+	@Override
+	public Employee elevate() {
+		return new Manager(getName(), getAge());
+	}
+	
+	@Override
+	public Employee degrement() {
+		return new Staff(getName(), getAge());
+	}
+}
---
+++ java_demo06/src/kr/co/exam03/Staff.java
@@ -0,0 +1,39 @@
+package kr.co.exam03;
+
+public class Staff extends Intern {
+
+	public Staff() {
+		role = "사원";
+	}
+	
+	public Staff(String name, int age) {
+		this();
+		setName(name);
+		setAge(age);
+	}
+	
+	public void research() {
+		String message = String.format("%s %s이(가) 자료 조사를 합니다.", role, getName());
+		System.out.println(message);
+	}
+	
+	public void document() {
+		String message = String.format("%s %s이(가) 문서 작성을 합니다.", role, getName());
+		System.out.println(message);
+	}
+	
+	public void care() {
+		String message = String.format("%s이(가) 인턴을 관리 합니다.", role);
+		System.out.println(message);
+	}
+	
+	@Override
+	public Employee elevate() {
+		return new Senior(getName(), getAge());
+	}
+	
+	@Override
+	public Employee degrement() {
+		return new Intern(getName(), getAge());
+	}
+}

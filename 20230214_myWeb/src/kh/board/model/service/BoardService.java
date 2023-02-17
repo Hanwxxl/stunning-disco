649393a44ba4e983cc2f86b5e3b242790a9a0e48
Hanwxxl <hanwxxl@gmail.com>
2023-02-17 12:31:08 +0900

    게시판 작업중

---
+++ 20230214_myWeb/src/kh/board/model/service/BoardService.java
@@ -0,0 +1,20 @@
+package kh.board.model.service;
+
+import java.sql.Connection;
+import java.util.List;
+
+import kh.board.model.dao.BoardDao;
+import kh.board.model.vo.BoardVo;
+import kh.common.jdbc.JdbcTemplate;
+
+public class BoardService {
+	
+	public List<BoardVo> getBoardList(){
+		List<BoardVo> result = null;
+		Connection conn = JdbcTemplate.getConnection();
+		result = new BoardDao().getBoardList(conn);
+		JdbcTemplate.close(conn);
+		return result;
+	}
+
+}
--- 20230214_myWeb/src/kh/home/controller/HomeController.java
+++ 20230214_myWeb/src/kh/home/controller/HomeController.java
@@ -1,11 +1,14 @@
 package kh.home.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import kh.board.model.service.BoardService;
 
 /**
  * Servlet implementation class HomeController
@@ -27,7 +30,8 @@ public class HomeController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//2.DB
-		request.setAttribute("boardlist", "TODO값");
+		//dto, dao, service, controller, view
+		request.setAttribute("boardlist", new BoardService().getBoardList());
 		request.getRequestDispatcher("/WEB-INF/view/index.jsp").forward(request, response);;
 		
 	}
